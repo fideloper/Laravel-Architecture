37dd50ae23c2a4bd9778a6f290171548d7e7db09
Fideloper <=>
2013-08-11 15:00:13 -0400

    updated readme with notes and started repo pattern'

---
+++ app/Arch/Repo/Article/ArticleInterface.php
@@ -0,0 +1,40 @@
+<?php namespace Arch\Repo\Article;
+
+interface ArticleInterface {
+
+    /**
+     * Get all articles
+     * @return \Illuminate\Support\Collection  Collection of Articles
+     */
+    public function all();
+
+    /**
+     * Get all articles per page
+     * @param  Integer $page Current Page
+     * @param  Integer $rpp  Results per page
+     * @return \Illuminate\Support\Collection  Collection of Articles
+     */
+    public function page($page=1, $rpp=10);
+
+    /**
+     * Get all articles per tag
+     * @param  string $tag Tag short name
+     * @return \Illuminate\Support\Collection  Collection of Articles
+     */
+    public function byTag($tag, $page=1, $rpp=10);
+
+    /**
+     * Get article by ID
+     * @param  Integer $id Article ID
+     * @return \Arch\Entity\Article     Single article
+     */
+    public function byId($id);
+
+    /**
+     * Get article by Short Name
+     * @param  String Article  Short Name
+     * @return \Arch\Entity\Article     Single article
+     */
+    public function byShortName($shortName);
+
+}
--- readme.md
+++ readme.md
@@ -2,3 +2,9 @@ Laravel-Architecture
 ====================
 
 Experimenting with code Architecture in Laravel
+
+## To Do
+1. Repository with Factories
+2. Move sql methods to Eloquent models from Repo (cleaner, but not necessarily necessary)
+3. Keep cache in Repo
+4. Extend collection for own Paginator and create or steal paginator interface. (Compose Laravel Paginator into extended class to do logic). Ability to set page manually, take Input class out of it.
